39f47f16e49a5909976cb6bb4bfc9ae95243e3b6
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-06-02 21:17:18 +0530

    chore: Update author name to divyanshrawat across all files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2026 Divyansh Rawat
+# Copyright 2026 divyanshrawat
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
