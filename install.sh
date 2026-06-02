3e667317d46f1a874fb7e5edef4c1479643f5c30
DsThakurRawat <divyanshrawatofficial@gmail.com>
2026-06-02 22:18:37 +0530

    chore: revert install.sh to point to main branch

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [ -d "$INSTALL_DIR" ]; then
     git pull
 else
     echo "Cloning Argus repository..."
-    git clone -b feature/cli-refactor https://github.com/DsThakurRawat/Argus.git "$INSTALL_DIR"
+    git clone https://github.com/DsThakurRawat/Argus.git "$INSTALL_DIR"
     cd "$INSTALL_DIR"
 fi
 
